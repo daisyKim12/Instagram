8f68cba46f48d221823c82b9aafc044857396546
daisyKim12 <ksjmichael12@gmail.com>
2023-03-06 00:04:52 +0900

    day2_second_post search fragment top edittext finished

--- app/src/main/java/com/project/Instargram/kotlin/src/main/search/SearchFragment.kt
+++ app/src/main/java/com/project/Instargram/kotlin/src/main/search/SearchFragment.kt
@@ -1,16 +1,38 @@
 package com.project.Instargram.kotlin.src.main.search
 
 import android.os.Bundle
-import android.view.LayoutInflater
 import android.view.View
-import android.view.ViewGroup
 import com.project.Instargram.kotlin.R
 import com.project.Instargram.kotlin.config.BaseFragment
 import com.project.Instargram.kotlin.databinding.FragmentSearchBinding
+
 
 class SearchFragment: BaseFragment<FragmentSearchBinding>(FragmentSearchBinding::bind, R.layout.fragment_search) {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+
+        changeEditText(false)
+
+        binding.edittextSearch.setOnFocusChangeListener { view, b -> changeEditText(b)}
+        binding.tbBack.setOnClickListener {
+            changeEditText(false)
+        }
+
+    }
+
+    private fun changeEditText(b: Boolean) {
+        if(b == true){
+            val params = binding.edittextSearch.getLayoutParams()
+            params.width = 880
+            binding.edittextSearch.setLayoutParams(params)
+            binding.tbBack.visibility = View.VISIBLE
+        } else {
+            val params = binding.edittextSearch.getLayoutParams()
+            params.width = 950
+            binding.edittextSearch.setLayoutParams(params)
+            binding.tbBack.visibility = View.INVISIBLE
+        }
+
     }
 }
---
+++ app/src/main/java/com/project/Instargram/kotlin/src/main/search/SearchGridFragment.kt
@@ -0,0 +1,14 @@
+package com.project.Instargram.kotlin.src.main.search
+
+import android.os.Bundle
+import android.view.View
+import com.project.Instargram.kotlin.R
+import com.project.Instargram.kotlin.config.BaseFragment
+import com.project.Instargram.kotlin.databinding.FragmentSearchGridBinding
+
+class SearchGridFragment: BaseFragment<FragmentSearchGridBinding>(FragmentSearchGridBinding::bind, R.layout.fragment_search_grid) {
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+    }
+}
