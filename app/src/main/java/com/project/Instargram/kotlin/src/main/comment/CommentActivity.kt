d062f9f5a2bed92df1d5e9ea4c5f0487486cfe09
daisyKim12 <ksjmichael12@gmail.com>
2023-03-11 13:25:08 +0900

    day8_first /commment activity rv item 완성

---
+++ app/src/main/java/com/project/Instargram/kotlin/src/main/comment/CommentActivity.kt
@@ -0,0 +1,12 @@
+package com.project.Instargram.kotlin.src.main.comment
+
+import android.os.Bundle
+import com.project.Instargram.kotlin.config.BaseActivity
+import com.project.Instargram.kotlin.databinding.ActivityCommentBinding
+
+class CommentActivity:BaseActivity<ActivityCommentBinding>(ActivityCommentBinding::inflate) {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+}
